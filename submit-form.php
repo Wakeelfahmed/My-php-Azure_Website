19d9d0a91e02038fff2a235d2d9f1b6add5a3162
Wakeel Furqan Ahmed <wakeelfahmed@gmail.com>
2023-03-19 01:43:30 +0500

    server response

---
+++ submit-form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Us - Confirmation</title>
+</head>
+<body>
+    <h1>Contact Us - Confirmation</h1>
+    <p>Thank you for your message, <?php echo $_POST['name']; ?>!</p>
+    <p>We have received your message and will respond to you at <?php echo $_POST['email']; ?> as soon as possible.</p>
+</body>
+</html>
